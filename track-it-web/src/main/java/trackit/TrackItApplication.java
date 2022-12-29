3f5eadb83741336a66d3c3806c28b8b47ba8585e
ht5929xp <ht.5929xp@gmail.com>
2022-12-29 16:00:27 -0500

    Fixes

---
+++ track-it-web/src/main/java/trackit/TrackItApplication.java
@@ -0,0 +1,11 @@
+package trackit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackItApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TrackItApplication.class, args);
+    }
+}
